5652faa7a6dc30aa8546abd006a4576a978bc012
mamad <mhmdshojaee@gmail.com>
2023-11-04 19:28:41 +0330

    add system resources route and model

--- app/api/v1/device/__init__.py
+++ app/api/v1/device/__init__.py
@@ -2,6 +2,7 @@ from .get_hostname import *
 from .get_info import *
 from .get_interface_by_name import *
 from .get_interfaces import *
+from .get_system_resources import *
 from .get_time import *
 from .set_hostname import *
 from .set_ip_settings import *
